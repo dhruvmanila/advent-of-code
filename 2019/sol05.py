401dfdb10498f722b3f9283f01abb513c9d5ebbd
Dhruv Manilawala <dhruvmanila@gmail.com>
2021-03-05 23:30:17 +0530

    (refactor): Renamed all files and minor changes

---
+++ 2019/sol05.py
@@ -0,0 +1,16 @@
+# https://adventofcode.com/2019/day/5
+
+
+if __name__ == "__main__":
+    import os.path
+
+    try:
+        from intcode import IntcodeComputer
+    except ImportError:
+        from .intcode import IntcodeComputer
+
+    with open(os.path.join(os.path.dirname(__file__), "input/05.txt")) as fd:
+        intcode_program = list(map(int, fd.readline().strip().split(",")))
+
+    print("System ID: 1 =>", IntcodeComputer(intcode_program, inputs=[1]).run())
+    print("System ID: 5 =>", IntcodeComputer(intcode_program, inputs=[5]).run())
