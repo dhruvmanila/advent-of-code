90fd3e8f3740fba7e7829772af925e2be1aa7abe
Dhruv Manilawala <dhruvmanila@gmail.com>
2021-12-25 14:07:22 +0530

    feat(go/set): additional methods and optimization

--- go/pkg/set/set.go
+++ go/pkg/set/set.go
@@ -1,82 +1,186 @@
-// Package set implements a set which is an unordered list of elements.
-//
-// This is a generic implementation where each value is contained within an
-// Element struct similar to that of the standard library container/list. To
-// access the underlying value, use the Value method.
+// Package set implements a set which is an unordered list of elements with no
+// duplicates.
 package set
 
 var exist = struct{}{}
 
 // Set represents an unordered list of elements.
 type Set struct {
-	list map[interface{}]struct{}
+	m map[interface{}]struct{}
 }
 
-// New returns an initialized Set.
+// New create and returns an initialized and empty Set.
 func New() *Set {
-	return &Set{list: make(map[interface{}]struct{})}
+	return NewWithSize(0)
 }
 
-// Add adds all the given values to the set.
-func (s *Set) Add(v ...interface{}) {
-	for _, i := range v {
-		s.list[i] = exist
+// NewWithSize create and returns an initialized and empty set with a given
+// size.
+func NewWithSize(size int) *Set {
+	return &Set{m: make(map[interface{}]struct{}, size)}
+}
+
+// NewWith create and returns a new set with the given elements.
+func NewWith(e ...interface{}) *Set {
+	return NewFromSlice(e)
+}
+
+// NewFromSlice create and returns a new set from an existing slice.
+func NewFromSlice(sl []interface{}) *Set {
+	s := NewWithSize(len(sl))
+	s.Add(sl...)
+	return s
+}
+
+// Add adds all the given elements to the set.
+func (s *Set) Add(es ...interface{}) {
+	for _, e := range es {
+		s.m[e] = exist
 	}
 }
 
-// Contains check if the given value exists in the set.
-func (s *Set) Contains(v interface{}) bool {
-	_, c := s.list[v]
+// Remove deletes all the given elements from the set. If there is no such
+// element, Remove is a no-op.
+func (s *Set) Remove(es ...interface{}) {
+	for _, e := range es {
+		delete(s.m, e)
+	}
+}
+
+// Contains check if the given element exists in the set.
+func (s *Set) Contains(e interface{}) bool {
+	_, c := s.m[e]
 	return c
 }
 
-// Remove deletes the value from the set. If there is no such value, Remove is
-// a no-op.
-func (s *Set) Remove(v interface{}) {
-	delete(s.list, v)
-}
-
-// Clear empties the set.
+// Clear removes all the elements from the set.
 func (s *Set) Clear() {
-	s.list = make(map[interface{}]struct{})
+	s.m = make(map[interface{}]struct{})
 }
 
 // Len returns the number of elements in the set.
 func (s *Set) Len() int {
-	return len(s.list)
+	return len(s.m)
 }
 
-// Union returns the union of s with other as a new set.
+// ForEach is used to iterate over every element of the set by calling a
+// user-defined function with every element.
+func (s *Set) ForEach(f func(e interface{})) {
+	for e := range s.m {
+		f(e)
+	}
+}
+
+// Iter is used to iterate over every element of the set. It returns a
+// receive-only buffered channel whose size is half of set length.
+func (s *Set) Iter() <-chan interface{} {
+	// Use a buffered channel to avoid blocking the main goroutine.
+	ch := make(chan interface{}, s.Len()/2)
+	go func() {
+		for e := range s.m {
+			ch <- e
+		}
+		close(ch)
+	}()
+	return ch
+}
+
+// Union returns a new set with elements from s and other.
 func (s *Set) Union(other *Set) *Set {
 	n := New()
-	for e := range s.list {
+	for e := range s.m {
 		n.Add(e)
 	}
-	for e := range other.list {
+	for e := range other.m {
 		n.Add(e)
 	}
 	return n
 }
 
-// Intersection returns an intersection of s with other as a new set.
+// Intersection returns a new set with elements common to s and other.
 func (s *Set) Intersection(other *Set) *Set {
 	n := New()
-	for e := range s.list {
-		if other.Contains(e) {
-			n.Add(e)
+	// Loop over the smaller set.
+	if s.Len() < other.Len() {
+		for e := range s.m {
+			if other.Contains(e) {
+				n.Add(e)
+			}
+		}
+	} else {
+		for e := range other.m {
+			if s.Contains(e) {
+				n.Add(e)
+			}
 		}
 	}
 	return n
 }
 
-// Difference returns the difference of s with other as a new set.
+// Difference returns a new set with elements in s that are not in other.
 func (s *Set) Difference(other *Set) *Set {
 	n := New()
-	for e := range s.list {
+	for e := range s.m {
 		if other.Contains(e) {
 			continue
 		}
 		n.Add(e)
 	}
 	return n
+}
+
+// SymmetricDifference returns a new set with elements in either s or other
+// but not in both.
+func (s *Set) SymmetricDifference(other *Set) *Set {
+	return s.Difference(other).Union(other.Difference(s))
+}
+
+// IsSubset returns true if every element in s is in other, false otherwise.
+func (s *Set) IsSubset(other *Set) bool {
+	if s.Len() > other.Len() {
+		return false
+	}
+	for e := range s.m {
+		if !other.Contains(e) {
+			return false
+		}
+	}
+	return true
+}
+
+// IsProperSubset returns true if s is a proper subset of other, that is, s is
+// a subset of other and length of s is strictly less than other.
+func (s *Set) IsProperSubset(other *Set) bool {
+	return s.IsSubset(other) && s.Len() < other.Len()
+}
+
+// IsSuperset returns true if every element in other is in s, false otherwise.
+func (s *Set) IsSuperset(other *Set) bool {
+	return other.IsSubset(s)
+}
+
+// IsProperSuperset returns true if s is a proper superset of other, that is,
+// s is a superset of other and length of s is strictly greater than other.
+func (s *Set) IsProperSuperset(other *Set) bool {
+	return s.IsSuperset(other) && s.Len() > other.Len()
+}
+
+// IsDisjoint returns true if s has no elements in common with other. Sets are
+// disjoint if and only if their intersection is an empty set.
+func (s *Set) IsDisjoint(other *Set) bool {
+	return s.Intersection(other).Len() == 0
+}
+
+// IsEqual returns true if s is equal to other. Sets are equal if their lengths
+// are equal and every element of s is in other.
+func (s *Set) IsEqual(other *Set) bool {
+	if s.Len() != other.Len() {
+		return false
+	}
+	for e := range s.m {
+		if !other.Contains(e) {
+			return false
+		}
+	}
+	return true
 }
