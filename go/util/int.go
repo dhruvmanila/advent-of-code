91b440579c561efc7cc2fb19bf3c320eaffae976
Dhruv Manilawala <dhruvmanila@gmail.com>
2021-12-25 14:07:49 +0530

    feat(go/int): digits iterator

--- go/util/int.go
+++ go/util/int.go
@@ -1,5 +1,7 @@
 package util
 
+import "strconv"
+
 // Sum is used to add all the integers in a given array.
 func Sum(arr []int) int {
 	total := 0
@@ -39,4 +41,29 @@ func IntMin(x, y int) int {
 		return x
 	}
 	return y
+}
+
+// Digits is used to iterate over each digit of the given number from left to
+// right. This returns a channel from which you can only receive an integer one
+// at a time and can be used in various ways like so:
+//
+//   for d := range Digits(123) {
+//     // do something with d
+//   }
+//
+//   ch := Digits(123)
+//   // ... other code
+//   fmt.Println(<-ch)
+//   // ... other code
+//   fmt.Println(<-ch)
+func Digits(n int) <-chan int {
+	ns := strconv.Itoa(n)
+	ch := make(chan int, len(ns))
+	go func() {
+		for _, d := range ns {
+			ch <- int(d - '0')
+		}
+		close(ch)
+	}()
+	return ch
 }
