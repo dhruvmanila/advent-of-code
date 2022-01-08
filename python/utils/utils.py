2d45d52d2f134dc2f2bcbadf646bba776660f1fd
Dhruv Manilawala <dhruvmanila@gmail.com>
2022-01-08 15:52:35 +0530

    feat(python): timing context

--- python/utils/utils.py
+++ python/utils/utils.py
@@ -1,4 +1,25 @@
+import contextlib
+import sys
+import time
 from pathlib import Path
+from typing import Generator
+
+
+@contextlib.contextmanager
+def timing(name: str = "") -> Generator[None, None, None]:
+    before = time.time()
+    try:
+        yield
+    finally:
+        after = time.time()
+        t = (after - before) * 1000
+        unit = "ms"
+        if t < 100:
+            t *= 1000
+            unit = "μs"
+        if name:
+            name = f" ({name})"
+        print(f"> {int(t)} {unit}{name}", file=sys.stderr, flush=True)
 
 
 def read(*, day: int, year: int, test: bool = False) -> str:
