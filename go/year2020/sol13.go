37e06edb21e78801f615b8f67b9fdd275933446a
Dhruv Manilawala <dhruvmanila@gmail.com>
2022-01-10 10:27:04 +0530

    feat(go): year2020 sol13 part2

--- go/year2020/sol13.go
+++ go/year2020/sol13.go
@@ -8,11 +8,10 @@ import (
 	"github.com/dhruvmanila/advent-of-code/go/util"
 )
 
-const sentinel = -1
-
-func earliestDeparture(earliest int, busIds []int) (earliestBus, wait int) {
+func earliestDeparture(earliest int, buses [][2]int) (earliestBus, wait int) {
 	wait = math.MaxInt
-	for _, bId := range busIds {
+	for _, bus := range buses {
+		bId := bus[0]
 		currentWait := int(math.Ceil(float64(earliest)/float64(bId)))*bId - earliest
 		if currentWait < wait {
 			wait = currentWait
@@ -22,6 +21,19 @@ func earliestDeparture(earliest int, busIds []int) (earliestBus, wait int) {
 	return earliestBus, wait
 }
 
+func earliestTimestamp(buses [][2]int) int {
+	t := 0
+	offset := 1
+	for _, bus := range buses {
+		bId, idx := bus[0], bus[1]
+		for ((t + idx) % bId) != 0 {
+			t += offset
+		}
+		offset *= bId
+	}
+	return t
+}
+
 func Sol13(input string) error {
 	lines, err := util.ReadLines(input)
 	if err != nil {
@@ -29,16 +41,16 @@ func Sol13(input string) error {
 	}
 
 	earliest := util.MustAtoi(lines[0])
-	var busIds []int
-	for _, s := range strings.Split(lines[1], ",") {
+	var buses [][2]int
+	for i, s := range strings.Split(lines[1], ",") {
 		if s == "x" {
 			continue
 		}
-		busIds = append(busIds, util.MustAtoi(s))
+		buses = append(buses, [2]int{util.MustAtoi(s), i})
 	}
 
-	earliestBus, wait := earliestDeparture(earliest, busIds)
+	earliestBus, wait := earliestDeparture(earliest, buses)
 
-	fmt.Printf("13.1: %d\n13.2: %d\n", earliestBus*wait, 0)
+	fmt.Printf("13.1: %d\n13.2: %d\n", earliestBus*wait, earliestTimestamp(buses))
 	return nil
 }
