3fd142007f20c7f54aecc703999c999300825c13
Dhruv Manilawala <dhruvmanila@gmail.com>
2022-01-15 21:34:09 +0530

    feat(go/pkg): update queue to use slice

--- go/pkg/queue/queue.go
+++ go/pkg/queue/queue.go
@@ -1,47 +1,72 @@
 package queue
 
-import "container/list"
+import "fmt"
 
 // Queue represents a simple queue data structure.
-type Queue struct {
-	list *list.List
+type Queue []interface{}
+
+// New returns an initialized queue, optionally with the given elements. The
+// elements are added in the same order as provided.
+func New(es ...interface{}) *Queue {
+	q := new(Queue)
+	if es != nil {
+		q.Enqueue(es...)
+	}
+	return q
 }
 
-// New returns an initialized and empty simple queue.
-func New() *Queue {
-	return &Queue{list: list.New()}
-}
-
-// Enqueue is used to enqueue an element to the queue.
-func (q *Queue) Enqueue(e interface{}) {
-	q.list.PushBack(e)
+// Enqueue is used to enqueue all the given elements to the queue. Multiple
+// elements are added in the same order as provided.
+func (q *Queue) Enqueue(es ...interface{}) {
+	*q = append(*q, es...)
 }
 
 // Dequeue is used to dequeue or remove the frontmost element from the queue
 // and returns it, or nil if the queue is empty.
 func (q *Queue) Dequeue() interface{} {
-	if e := q.list.Front(); e != nil {
-		q.list.Remove(e)
-		return e.Value
+	sl := *q
+	if len(sl) == 0 {
+		return nil
 	}
-	return nil
+	e := sl[0]
+	if len(sl) == 1 {
+		// Clear the slice
+		*q = nil
+	} else {
+		*q = sl[1:]
+	}
+	return e
 }
 
 // Peek returns the frontmost element of the queue without removing it, or nil
 // if the queue is empty.
 func (q *Queue) Peek() interface{} {
-	if e := q.list.Front(); e != nil {
-		return e.Value
+	sl := *q
+	if len(sl) == 0 {
+		return nil
 	}
-	return nil
+	return sl[0]
 }
 
 // Len returns the number of elements in the queue.
 func (q *Queue) Len() int {
-	return q.list.Len()
+	return len(*q)
 }
 
 // IsEmpty is used to check whether the queue is empty or not.
 func (q *Queue) IsEmpty() bool {
 	return q.Len() == 0
+}
+
+// ToSlice returns a slice containing the elements of the queue where the first
+// element is the start of the queue. Mutating the returned slice will not
+// affect the underlying implementation.
+func (q *Queue) ToSlice() []interface{} {
+	sl := make([]interface{}, 0, q.Len())
+	copy(sl, *q)
+	return sl
+}
+
+func (q *Queue) String() string {
+	return fmt.Sprintf("Queue%v", *q)
 }
