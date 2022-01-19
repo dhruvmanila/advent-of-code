235e6080a0420f9a748453295c5de7c699273207
Dhruv Manilawala <dhruvmanila@gmail.com>
2022-01-19 23:40:50 +0530

    feat(c): year2015 sol1

--- c/year2015/sol01.c
+++ c/year2015/sol01.c
@@ -1,5 +1,29 @@
+#include <stdint.h>
 #include <stdio.h>
+#include <string.h>
 
 void year2015_sol01(char *input) {
-  printf("ANSWER: %s\n", input);
+  FILE *fp = fopen(input, "r");
+  if (fp == NULL) {
+    fprintf(stderr, "unable to open file: %s\n", input);
+  }
+
+  char *line = NULL;
+  size_t linecap = 0;
+  getline(&line, &linecap, fp);
+  fclose(fp);
+
+  int16_t floor = 0;
+  uint16_t position = 0;
+  for (uint16_t i = 0; i < (uint16_t)strlen(line); i++) {
+    switch (line[i]) {
+    case '(': floor++; break;
+    case ')': floor--; break;
+    }
+    if (floor == -1 && position == 0) {
+      position = i + 1;
+    }
+  }
+
+  printf("1.1: %d\n1.2: %d\n", floor, position);
 }
