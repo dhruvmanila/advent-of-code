357ef2b22173e29faf5d0241d5a0ec41c151c8ec
Dhruv Manilawala <dhruvmanila@gmail.com>
2022-01-20 22:33:00 +0530

    feat(c): year2015 sol02

---
+++ c/year2015/sol02.c
@@ -0,0 +1,52 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "../lib/read.h"
+
+// PRINT_BOX is a macro to print the box_t struct for debugging purposes.
+#define PRINT_BOX(b)                                                            \
+  fprintf(stdout, "box{length: %d width: %d height: %d}\n", b.length, b.width,  \
+          b.height)
+
+#define MIN(a, b) ((a < b) ? a : b)
+
+typedef struct {
+  int length;
+  int width;
+  int height;
+} box_t;
+
+int wrapping_paper_area(box_t box) {
+  int lw = box.length * box.width;
+  int wh = box.width * box.height;
+  int hl = box.height * box.length;
+  return 2*lw + 2*wh + 2*hl + MIN(MIN(lw, wh), hl);
+}
+
+int ribbon_length(box_t box) {
+  int length = 0;
+  int lw_min = MIN(box.length, box.width);
+  if (lw_min == box.length) {
+    length = lw_min * 2 + MIN(box.width, box.height) * 2;
+  } else {
+    length = lw_min * 2 + MIN(box.length, box.height) * 2;
+  }
+  return box.length * box.width * box.height + length;
+}
+
+int year2015_sol02(char *input) {
+  char **lines = NULL;
+  int n = readlines(&lines, input);
+  int area = 0, ribbon = 0;
+  for (int i = 0; i < n; i++) {
+    box_t box;
+    sscanf(lines[i], "%dx%dx%d", &box.length, &box.width, &box.height);
+    area += wrapping_paper_area(box);
+    ribbon += ribbon_length(box);
+  }
+
+  printf("2.1: %d\n2.2: %d\n", area, ribbon);
+  return EXIT_SUCCESS;
+}
--- c/year2015/solutions.h
+++ c/year2015/solutions.h
@@ -1,3 +1,4 @@
 #pragma once
 
 int year2015_sol01(char *);
+int year2015_sol02(char *);
