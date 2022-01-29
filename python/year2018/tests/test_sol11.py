851deae8f9948f6686ebefa9431a0e2e23310b75
Dhruv Manilawala <dhruvmanila@gmail.com>
2022-01-29 11:58:02 +0530

    feat(python): year2018 sol11

---
+++ python/year2018/tests/test_sol11.py
@@ -0,0 +1,16 @@
+import pytest
+
+from year2018.sol11 import power_level
+
+
+@pytest.mark.parametrize(
+    ("x", "y", "serial_number", "expected"),
+    (
+        (3, 5, 8, 4),
+        (122, 79, 57, -5),
+        (217, 196, 39, 0),
+        (101, 153, 71, 4),
+    ),
+)
+def test_power_level(x: int, y: int, serial_number: int, expected: int):
+    assert power_level(x, y, serial_number) == expected
