ca9c729f9d70b9fbc95d4da4279b3d7d2b01e191
Dhruv Manilawala <dhruvmanila@gmail.com>
2022-12-01 10:37:58 +0530

    feat(go): year2022 sol01

--- go/year2022/sol01.go
+++ go/year2022/sol01.go
@@ -2,16 +2,30 @@ package year2022
 
 import (
 	"fmt"
+	"sort"
 
 	"github.com/dhruvmanila/advent-of-code/go/util"
 )
 
 func Sol01(input string) error {
-	_, err := util.ReadLines(input)
+	sections, err := util.ReadSections(input)
 	if err != nil {
 		return err
 	}
 
-	fmt.Printf("1.1: %d\n1.2: %d\n", 0, 0)
+	var elves []int
+	for _, lines := range sections {
+		calories := 0
+		for _, line := range lines {
+			calories += util.MustAtoi(line)
+		}
+		elves = append(elves, calories)
+	}
+
+	sort.Slice(elves, func(i, j int) bool {
+		return elves[i] > elves[j]
+	})
+
+	fmt.Printf("1.1: %d\n1.2: %d\n", elves[0], elves[0]+elves[1]+elves[2])
 	return nil
 }
