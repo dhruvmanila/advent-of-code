f960f01f1d3c163826a60a628b0317cecc3e972a
Dhruv Manilawala <dhruvmanila@gmail.com>
2022-12-09 08:05:57 +0530

    refactor: use matrix for year2022 sol08

--- go/year2022/sol08.go
+++ go/year2022/sol08.go
@@ -3,17 +3,18 @@ package year2022
 import (
 	"fmt"
 
+	"github.com/dhruvmanila/advent-of-code/go/pkg/matrix"
 	"github.com/dhruvmanila/advent-of-code/go/util"
 )
 
 // heightMap represents a map with the height of each tree.
 type heightMap struct {
 	// height is a matrix to store the tree height at (row, column).
-	height [][]byte
+	height *matrix.Dense[byte]
 
 	// scenicScore is a matrix to store the scenic score for the tree
 	// at (row, column).
-	scenicScore [][]int
+	scenicScore *matrix.Dense[int]
 
 	// rows and columns are the total number of rows and columns in the grid.
 	rows    int
@@ -21,27 +22,20 @@ type heightMap struct {
 }
 
 // newHeightMap creates a new height map from the given lines.
-//
-// Internally, the grid is represented as byte slices.
 func newHeightMap(lines []string) *heightMap {
 	rows, columns := len(lines), len(lines[0])
 
-	height := make([][]byte, rows)
-	for idx, line := range lines {
-		height[idx] = []byte(line)
-	}
-
-	// Initialize a 0-slice for adding the scenic score. As it's initialized
-	// to 0, make sure to not use this value when computing the score otherwise
-	// the value will always be 0.
-	scenicScore := make([][]int, rows)
-	for i := 0; i < rows; i++ {
-		scenicScore[i] = make([]int, columns)
+	height := make([]byte, 0, rows*columns)
+	for _, line := range lines {
+		height = append(height, []byte(line)...)
 	}
 
 	return &heightMap{
-		height:      height,
-		scenicScore: scenicScore,
+		height: matrix.NewDense(rows, columns, height),
+		// Initialize a 0-slice for adding the scenic score. As it's initialized
+		// to 0, make sure to not use this value when computing the score otherwise
+		// the value will always be 0.
+		scenicScore: matrix.NewDense[int](rows, columns, nil),
 		rows:        rows,
 		columns:     columns,
 	}
@@ -52,7 +46,7 @@ func newHeightMap(lines []string) *heightMap {
 // from (row, col) to the top side.
 func (t *heightMap) topVisibility(row, col int, height byte) (bool, int) {
 	for r := row - 1; r >= 0; r-- {
-		if t.height[r][col] >= height {
+		if t.height.At(r, col) >= height {
 			return false, row - r
 		}
 	}
@@ -64,7 +58,7 @@ func (t *heightMap) topVisibility(row, col int, height byte) (bool, int) {
 // from (row, col) to the left side.
 func (t *heightMap) leftVisibility(row, col int, height byte) (bool, int) {
 	for c := col - 1; c >= 0; c-- {
-		if t.height[row][c] >= height {
+		if t.height.At(row, c) >= height {
 			return false, col - c
 		}
 	}
@@ -76,7 +70,7 @@ func (t *heightMap) leftVisibility(row, col int, height byte) (bool, int) {
 // from (row, col) to the bottom side.
 func (t *heightMap) bottomVisibility(row, col int, height byte) (bool, int) {
 	for r := row + 1; r < t.rows; r++ {
-		if t.height[r][col] >= height {
+		if t.height.At(r, col) >= height {
 			return false, r - row
 		}
 	}
@@ -88,7 +82,7 @@ func (t *heightMap) bottomVisibility(row, col int, height byte) (bool, int) {
 // from (row, col) to the right side.
 func (t *heightMap) rightVisibility(row, col int, height byte) (bool, int) {
 	for c := col + 1; c < t.columns; c++ {
-		if t.height[row][c] >= height {
+		if t.height.At(row, c) >= height {
 			return false, c - col
 		}
 	}
@@ -104,14 +98,14 @@ func (t *heightMap) VisibleCount() int {
 
 	for r := 1; r < t.rows-1; r++ {
 		for c := 1; c < t.columns-1; c++ {
-			height := t.height[r][c]
+			height := t.height.At(r, c)
 
 			isVisibleFromTop, viewingDistanceTop := t.topVisibility(r, c, height)
 			isVisibleFromLeft, viewingDistanceLeft := t.leftVisibility(r, c, height)
 			isVisibleFromBottom, viewingDistanceBottom := t.bottomVisibility(r, c, height)
 			isVisibleFromRight, viewingDistanceRight := t.rightVisibility(r, c, height)
 
-			t.scenicScore[r][c] = viewingDistanceTop * viewingDistanceLeft * viewingDistanceBottom * viewingDistanceRight
+			t.scenicScore.Set(r, c, viewingDistanceTop*viewingDistanceLeft*viewingDistanceBottom*viewingDistanceRight)
 			if isVisibleFromTop || isVisibleFromLeft || isVisibleFromBottom || isVisibleFromRight {
 				count++
 			}
@@ -126,17 +120,15 @@ func (t *heightMap) VisibleCount() int {
 // method to compute the scores.
 func (t *heightMap) MaxScore() int {
 	maxScore := 0
-	for _, scores := range t.scenicScore {
-		for _, score := range scores {
-			maxScore = util.Max(maxScore, score)
-		}
+	for _, score := range t.scenicScore.Data {
+		maxScore = util.Max(maxScore, score)
 	}
 	return maxScore
 }
 
 func (t *heightMap) String() string {
 	s := ""
-	for _, row := range t.height {
+	for _, row := range t.height.Data {
 		s += string(row) + "\n"
 	}
 	return s
