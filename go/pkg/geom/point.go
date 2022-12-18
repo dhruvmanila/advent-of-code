4efd30f10508293dd9e4cb261584f6a9a6f7fb0b
Dhruv Manilawala <dhruvmanila@gmail.com>
2022-12-18 16:42:39 +0530

    feat: Point.Neighbors to get neighboring points
    Limit the type constraint to be signed to avoid integer underflow.

--- go/pkg/geom/point.go
+++ go/pkg/geom/point.go
@@ -8,8 +8,31 @@ import (
 	"github.com/dhruvmanila/advent-of-code/go/util"
 )
 
+// Directions2D is an array of points corresponding to the difference to
+// move in a certain direction in 2D. The order of the points is clockwise
+// starting from up, i.e., UP, RIGHT, DOWN, LEFT.
+var Directions2D = [4]Point2D[int]{
+	{0, -1}, // UP
+	{1, 0},  // RIGHT
+	{0, 1},  // DOWN
+	{-1, 0}, // LEFT
+}
+
+// Directions3D is an array of points corresponding to the difference to
+// move in a certain direction in 3D. The order of the points is clockwise
+// starting from up, and then front and back (z-axis), i.e., UP, RIGHT, DOWN,
+// LEFT, FRONT, BACK.
+var Directions3D = [6]Point3D[int]{
+	{0, -1, 0}, // UP
+	{1, 0, 0},  // RIGHT
+	{0, 1, 0},  // DOWN
+	{-1, 0, 0}, // LEFT
+	{0, 0, 1},  // FRONT
+	{0, 0, -1}, // BACK
+}
+
 // Point2D represents a 2 dimensional point in the coordinate system.
-type Point2D[T constraints.Integer] struct {
+type Point2D[T constraints.Signed] struct {
 	X, Y T
 }
 
@@ -37,12 +60,25 @@ func (p Point2D[T]) ManhattanDistance(other Point2D[T]) T {
 	return util.Abs(p.X-other.X) + util.Abs(p.Y-other.Y)
 }
 
+// Neighbors returns the neighboring points for p. These are the 6 directions
+// corresponding to +ve and -ve X, Y and Z axis.
+func (p Point2D[T]) Neighbors() []Point2D[T] {
+	neighbors := make([]Point2D[T], 0, len(Directions2D))
+	for _, direction := range Directions2D {
+		neighbors = append(neighbors, Point2D[T]{
+			X: p.X + T(direction.X),
+			Y: p.X + T(direction.Y),
+		})
+	}
+	return neighbors
+}
+
 func (p Point2D[T]) String() string {
 	return fmt.Sprintf("(%d, %d)", p.X, p.Y)
 }
 
 // Point3D represents a 3 dimensional point in the coordinate system.
-type Point3D[T constraints.Integer] struct {
+type Point3D[T constraints.Signed] struct {
 	X, Y, Z T
 }
 
@@ -65,6 +101,25 @@ func (p Point3D[T]) Sub(other Point3D[T]) Point3D[T] {
 // Equal returns true if p and other are the same point.
 func (p Point3D[T]) Equal(other Point3D[T]) bool {
 	return p.X == other.X && p.Y == other.Y && p.Z == other.Z
+}
+
+// ManhattanDistance returns the manhattan distance between p and other.
+func (p Point3D[T]) ManhattanDistance(other Point3D[T]) T {
+	return util.Abs(p.X-other.X) + util.Abs(p.Y-other.Y) + util.Abs(p.Z-other.Z)
+}
+
+// Neighbors returns the neighboring points for p. These are the 6 directions
+// corresponding to +ve and -ve X, Y and Z axis.
+func (p Point3D[T]) Neighbors() []Point3D[T] {
+	neighbors := make([]Point3D[T], 0, len(Directions3D))
+	for _, direction := range Directions3D {
+		neighbors = append(neighbors, Point3D[T]{
+			X: p.X + T(direction.X),
+			Y: p.Y + T(direction.Y),
+			Z: p.Z + T(direction.Z),
+		})
+	}
+	return neighbors
 }
 
 func (p Point3D[T]) String() string {
