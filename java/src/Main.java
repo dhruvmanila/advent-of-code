a725c4f9729f281df0aa99040d567004becd3a75
Dhruv Manilawala <dhruvmanila@gmail.com>
2025-12-06 09:52:49 +0530

    Remove Java solution

--- java/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        try {
-            Year2022.Day01.Solve();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-}
